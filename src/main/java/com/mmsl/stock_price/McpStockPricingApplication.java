66b5f906c6ed16d91a35124ba26e2c86a851e490
Marina Leide <marina.msleide@gmail.com>
2025-09-06 19:17:39 -0300

    Creating a repository to the MCP project

---
+++ src/main/java/com/mmsl/stock_price/McpStockPricingApplication.java
@@ -0,0 +1,13 @@
+package com.mmsl.stock_price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpStockPricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpStockPricingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmsl/stock_price/McpStockPricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmsl.stock_price;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpStockPricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
